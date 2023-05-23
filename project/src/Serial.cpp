47fcea48c89dbc56470dca9f4b7e6d7840425230
Wonderland23333 <callas-holier.0f@icloud.com>
2023-05-23 21:35:56 +0800

    fix Srial baud

--- project/src/Serial.cpp
+++ project/src/Serial.cpp
@@ -4,7 +4,28 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <termios.h>
+using namespace std;
 
+int convert_baud(string a){
+    if(a == "B0")return 0;
+    else if(a == "B50")return 1;
+    else if (a == "B75") return 2;
+    else if (a == "B110") return 3;
+    else if (a == "B134") return 4;
+    else if (a == "B150") return 5;
+    else if (a == "B200") return 6;
+    else if (a == "B300") return 7;
+    else if (a == "B600") return 8;
+    else if (a == "B1200") return 9;
+    else if (a == "B1800") return 10;
+    else if (a == "B2400") return 11;
+    else if (a == "B4800") return 12;
+    else if (a == "B9600") return 13;
+    else if (a == "B19200") return 14;
+    else if (a == "B38400") return 15;
+
+    return -1;
+}
 // 串口配置
 const char* SERIAL_PORT = "/dev/ttyUSB0";
 const int SERIAL_BAUDRATE = B9600;
@@ -20,7 +41,7 @@ public:
     bool openSerialPort(char* serial_port, int serial_baudrate) {
         fd = open(serial_port, O_RDWR | O_NOCTTY | O_NDELAY);
         if (fd == -1) {
-            std::cerr << "Failed to open serial port." << std::endl;
+            cerr << "Failed to open serial port." << endl;
             return false;
         }
 
@@ -42,9 +63,9 @@ public:
         }
     }
 
-    bool sendSerialData(const std::string& data) {
+    bool sendSerialData(const string& data) {
         if (fd == -1) {
-            std::cerr << "Serial port is not open." << std::endl;
+            cerr << "Serial port is not open." << endl;
             return false;
         }
 
@@ -58,24 +79,31 @@ int main() {
 
     // 用户设置通信参数示例
     // TODO: 根据用户需求设置波特率、数据位、停止位等
-    printf("请输入波特率：（形式：B波特率）\n");
-    int baud_rate;
-    std::cin>>baud_rate;
+    G:printf("请输入波特率：(B+波特率)\n");
+    string baud_rate_c;
+    cin>>baud_rate_c;
+
+    int baud_rate = convert_baud(baud_rate_c);
+    if(baud_rate == -1){
+        cout<<"worry baud rate!"<<endl;
+        goto G;
+    }
+
     printf("请输入串口设备文件路径：\n");
     char* serial_path;
-    std::cin>>serial_path;
-
+    cin>>serial_path;
+    
     // 打开串口
     if (!serialComm.openSerialPort(serial_path,baud_rate)) {
-        std::cerr << "Failed to open serial port. Exiting." << std::endl;
+        cerr << "Failed to open serial port. Exiting." << endl;
         return 1;
     }
 
-    std::string message = "Hello, World!";
+    string message = "Hello, World!";
 
     // 发送数据示例
     if (!serialComm.sendSerialData(message)) {
-        std::cerr << "Failed to send data through serial port." << std::endl;
+        cerr << "Failed to send data through serial port." << endl;
     }
 
     // 关闭串口
